dacbd6fd61070977fa0be021004aa6d930e66109
El-Juanpis <juanpablor629@gmail.com>
2021-10-28 14:45:00 -0500

    resto de carpetas siuuu

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por "juan alias el copias"
+    </div>
+    <div>
+      <a href="https://www.instagram.com/juanpablor629/">Sigueme en instagram</a><br></br>
+      <p>no subo nada pero seguime</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
